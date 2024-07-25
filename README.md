6bfd519fb89eed09e6b55604cfadea69786c6590
amanchopra1905 <amanc@lambdatest.com>
2024-07-25 13:30:37 +0530

    updated the test files and the yaml for k6 hyp

--- README.md
+++ README.md
@@ -1,1 +1,52 @@
-# HYP-K6-browser-sample
+# How to run HyperExecute k6 Sample?
+
+k6 is an open-source tool designed to make load testing easy and accessible for developers and QA engineers. It's specifically built for testing the performance of APIs, microservices, and websites.
+
+## Prerequisites
+To run the Tests on HyperExecute from your Local System, you are required:
+
+- Your LambdaTest [Username and Access key](https://www.lambdatest.com/support/docs/hyperexecute-how-to-get-my-username-and-access-key/)
+- [HyperExecute YAML](https://www.lambdatest.com/support/docs/hyperexecute-yaml-parameters/) file which contains all the necessary instructions.
+- [HyperExecute CLI](https://www.lambdatest.com/support/docs/hyperexecute-cli-run-tests-on-hyperexecute-grid/) in order to initiate a test execution Job .
+
+## Step 1: Downlaod the HyperExecute CLI
+
+You can download the `HyperExecute CLI` for your OS from the links given below. The CLI should be kept in the root folder of your test suite.
+
+| Platform | Download Link |
+| ---------| ------------- |
+| Linux | https://downloads.lambdatest.com/hyperexecute/linux/hyperexecute |
+| Windows | https://downloads.lambdatest.com/hyperexecute/windows/hyperexecute.exe |
+| macOS | https://downloads.lambdatest.com/hyperexecute/darwin/hyperexecute |
+
+## Step 2: Configure your YAML file
+
+You will have to add these flags mandatorily in your YAML file to execute the k6 tests:
+
+- [runtime flag](https://www.lambdatest.com/support/docs/deep-dive-into-hyperexecute-yaml/#runtime) to download and install the k6 of that respective version.
+
+```yaml
+runtime:
+    addons:
+      - name: k6
+        version: "v0.52.0"
+```
+
+- [env flag](https://www.lambdatest.com/support/docs/deep-dive-into-hyperexecute-yaml/#env) is used to install certain dependencies to run your test cases
+
+```yaml
+env: 
+  K6_BROWSER_ENABLED: true  #Indicates whether browser emulation is enabled for the k6 test.
+  K6_BROWSER_HEADLESS: false #Determines if the browser should run in headless mode
+  HE_CONTEXT_K6_SETUP_DEFAULT_BROWSER_PATH: true
+```
+
+## Step 3: Execute your Test Suite
+
+> **NOTE:** In case of macOS, if you get a permission denied warning while executing CLI, simply run `chmod u+x ./hyperexecute` to allow permission. In case you get a security popup, allow it from your **System Preferences → Security & Privacy → General tab**.
+
+Run the below command in your terminal at the root folder of the project:
+
+```bash
+./hyperexecute --user YOUR_USERNAME --key YOUR_ACCESS_KEY --config RELATIVE_PATH_OF_YOUR_YAML_FILE
+```
---
+++ tests/test1.js
@@ -0,0 +1,79 @@
+import { browser } from 'k6/browser';
+import { check } from 'k6';
+
+export const options = {
+  scenarios: {
+    ui: {
+      executor: 'shared-iterations',
+      options: {
+        browser: {
+          type: 'chromium',
+        },
+      },
+    },
+  },
+  thresholds: {
+    checks: ['rate==1.0'],
+  },
+};
+
+export default async function () {
+  const context = await browser.newContext();
+  const page = await context.newPage();
+
+  try {
+    // Test: Search LambdaTest on Bing and navigate Playwright documentation
+    await page.setViewportSize({ width: 1920, height: 1080 });
+    
+    await page.goto('https://playwright.dev/');
+    check(await page.title(), {
+      'Playwright: page title is correct': (t) => /Playwright/.test(t),
+    });
+
+    await sleep(9000);
+    let getStartedLink = await page.locator('text=Get Started').first();
+    check(await getStartedLink.getAttribute('href'), {
+      'Playwright: Get Started link is correct': (href) => href === '/docs/intro',
+    });
+    await getStartedLink.click();
+
+    check(await page.locator('text=Installation').first().isVisible(), {
+      'Playwright: Installation is visible': (isVisible) => isVisible,
+    });
+
+    const topics = [
+      'Trace Viewer', 'Test Generator', 'Release notes', 'Annotations', 'API testing', 'Authentication', 
+      'Command line', 'Configuration', 'Page Object Model', 'Parameterize tests', 'Reporters', 'Retries', 
+      'Timeouts', 'Visual comparisons', 'Fixtures', 'TypeScript', 'Components (experimental)', 'Library', 
+      'Auto-waiting', 'Browsers', 'Chrome Extensions', 'Dialogs', 'Downloads', 'Emulation', 
+      'Evaluating JavaScript', 'Events', 'Extensibility', 'Frames', 'Handles', 'Locators', 
+      'Navigations', 'Network', 'Pages', 'Page Object Models', 'Screenshots', 'Videos', 
+      'Migration', 'Migrating from Protractor', 'Integrations', 'Docker', 'Continuous Integration', 
+      'Selenium Grid', 'Supported languages'
+    ];
+
+    for (const topic of topics) {
+      await page.locator(`text=${topic}`).click();
+    }
+
+    await page.goto('https://www.bing.com');
+    let searchBox = await page.locator('[id="sb_form_q"]');
+    await searchBox.click();
+    await searchBox.type('LambdaTest');
+    await searchBox.press('Enter');
+
+    let title = await page.title();
+    console.log('Page title:: ', title);
+    
+    check(title, {
+      'Bing: search results title contains LambdaTest': (t) => t.includes('LambdaTest'),
+    });
+
+  } finally {
+    await page.close();
+  }
+}
+
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
