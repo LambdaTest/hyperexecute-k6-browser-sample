17f2c95e137e59a5902621bfdf2ec14adf698861
amanchopra1905 <amanc@lambdatest.com>
2024-07-29 19:00:12 +0530

    added all the files

---
+++ tests/test3.js
@@ -0,0 +1,47 @@
+import { browser } from 'k6/browser';
+import { check } from 'k6';
+
+export const options = {
+  scenarios: {
+    ui: {
+      executor: 'shared-iterations',
+      options: {
+        browser: {
+          type: 'chromium',
+        },
+      },
+    },
+  },
+  thresholds: {
+    checks: ['rate==1.0'],
+  },
+};
+
+export default async function () {
+  const context = await browser.newContext();
+  const page = await context.newPage();
+
+  try {
+    await page.goto("https://test.k6.io/my_messages.php");
+
+    await page.locator('input[name="login"]').type("admin");
+    await page.locator('input[name="password"]').type("123");
+
+    await Promise.all([
+      page.waitForNavigation(),
+      page.locator('input[type="submit"]').click(),
+    ]);
+    
+    await sleep(5000);
+    const header = await page.locator("h2").textContent();
+    check(header, {
+      header: (h) => h == "Welcome, admin!",
+    });
+  } finally {
+    await page.close();
+  }
+}
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
